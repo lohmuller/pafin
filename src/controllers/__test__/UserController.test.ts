0045f5d6e50feb3b495e6431b1aa36bdf1b9066d
lohmuller <hidden@hotmail.com>
2023-12-19 15:28:19 +0900

    Better unit test names

--- src/controllers/__test__/UserController.test.ts
+++ src/controllers/__test__/UserController.test.ts
@@ -1,21 +1,19 @@
-import request, { Options } from 'supertest';
-import { app, appServer } from '../../app';
+import request from 'supertest';
+import { app } from '../../app';
 import { NextFunction } from 'express';
 import UserModel from '../../models/UserModel';
 
-
 jest.mock('../../models/UserModel');
 
-describe('UserController Get Users', () => {
-
-    it('should get all users', async () => {
+describe('UserController - Get Users', () => {
+    it('should retrieve all users successfully', async () => {
         UserModel.findAll = jest.fn().mockResolvedValueOnce([createMockUser(), createMockUser()]);
         const response = await request(app).get('/users/');
         expect(response.status).toBe(200);
         expect(response.body.length).toEqual(2);
     });
 
-    it('should get a user by ID', async () => {
+    it('should retrieve a user by ID successfully', async () => {
         UserModel.findByPk = jest.fn().mockResolvedValueOnce(createMockUser());
         const userId = 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab';
         const response = await request(app).get(`/users/${userId}`);
@@ -27,7 +25,7 @@ describe('UserController Get Users', () => {
         });
     });
 
-    it('User not found by id', async () => {
+    it('should handle user not found by ID', async () => {
         UserModel.findByPk = jest.fn().mockResolvedValueOnce(null);
         const userId = 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab';
         const response = await request(app).get(`/users/${userId}`);
@@ -36,9 +34,8 @@ describe('UserController Get Users', () => {
     });
 });
 
-describe('UserController - CRUD Operation', () => {
-
-    it('should create a user', async () => {
+describe('UserController - CRUD Operations', () => {
+    it('should create a new user successfully', async () => {
         const userData = {
             "email": "different@email.com",
             "password": "123456",
@@ -46,7 +43,7 @@ describe('UserController - CRUD Operation', () => {
         };
 
         UserModel.create = jest.fn().mockResolvedValueOnce(createMockUser(userData));
-        const response = (await request(app).post('/users/').send(userData));
+        const response = await request(app).post('/users/').send(userData);
         expect(response.status).toBe(201);
         expect(response.body).toEqual({
             id: 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab',
@@ -56,11 +53,11 @@ describe('UserController - CRUD Operation', () => {
         });
     });
 
-    it('should update a user', async () => {
+    it('should update an existing user successfully', async () => {
         UserModel.findByPk = jest.fn().mockResolvedValueOnce(createMockUser());
         UserModel.update = jest.fn().mockResolvedValueOnce(null);
         const userId = 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab';
-        const response = (await request(app).put(`/users/${userId}`).send({ name: "test" }));
+        const response = await request(app).put(`/users/${userId}`).send({ name: "test" });
         expect(response.status).toBe(200);
         expect(response.body).toEqual({
             id: userId,
@@ -69,7 +66,7 @@ describe('UserController - CRUD Operation', () => {
         });
     });
 
-    it('should delete a user', async () => {
+    it('should delete a user successfully', async () => {
         UserModel.findByPk = jest.fn().mockResolvedValueOnce(createMockUser());
         UserModel.destroy = jest.fn().mockResolvedValueOnce(null);
         const userId = 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab';
@@ -77,27 +74,25 @@ describe('UserController - CRUD Operation', () => {
         expect(response.status).toBe(204);
     });
 
-
-    it('should show error while create a user', async () => {
-        const response = (await request(app).post('/users/').send({}));
+    it('should handle validation error while creating a user', async () => {
+        const response = await request(app).post('/users/').send({});
         expect(response.status).toBe(400);
     });
 
-    it('should show user not found when update a user', async () => {
+    it('should show user not found error when updating a user', async () => {
         const userId = 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab';
         const response = await request(app).put(`/users/${userId}`).send({ name: "test" });
         expect(response.status).toBe(404);
         expect(response.body).toEqual({ "error": "User not found." });
     });
 
-    it('should not update a user', async () => {
+    it('should not update a user with invalid ID', async () => {
         const userId = 'b';
         const response = await request(app).put(`/users/${userId}`).send();
         expect(response.status).toBe(400);
-        //expect(response.body).toEqual({ "error": "User not found." });
     });
 
-    it('should delete a user', async () => {
+    it('should handle user not found error when deleting a user', async () => {
         const userId = 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab';
         const response = await request(app).delete(`/users/${userId}`);
         expect(response.status).toBe(404);
@@ -105,7 +100,7 @@ describe('UserController - CRUD Operation', () => {
 });
 
 describe('UserController - Authentication', () => {
-    it('should authenticate a user', async () => {
+    it('should authenticate a user successfully', async () => {
         UserModel.findOne = jest.fn().mockResolvedValueOnce(createMockUser());
         const response = await request(app).post('/auth/login').send({
             "email": "aaa@bb.com",
@@ -114,7 +109,7 @@ describe('UserController - Authentication', () => {
         expect(response.status).toBe(200);
     });
 
-    it('should show wrong password while login', async () => {
+    it('should show wrong password error during login', async () => {
         UserModel.findOne = jest.fn().mockResolvedValueOnce(createMockUser({}, false));
         const response = await request(app).post('/auth/login').send({
             "email": "aaa@bb.com",
@@ -123,13 +118,13 @@ describe('UserController - Authentication', () => {
         expect(response.status).toBe(401);
     });
 
-    it('should not find a user while login', async () => {
+    it('should handle user not found error during login', async () => {
         const response = await request(app).post('/auth/login').send({});
         expect(response.status).toBe(401);
     });
 });
 
-//Set this as default logged user
+// Set the default logged-in user for authentication
 jest.mock('../../middleware/authMiddleware', () => ({
     authenticateJWT: (req: any, res: any, next: NextFunction) => {
         req.user = {
@@ -144,7 +139,7 @@ afterAll(() => {
     jest.clearAllMocks();
 });
 
-const createMockUser = (overrides?: object, comparePassword: boolean = true) => ({
+const createMockUser = (overrides?: Partial<UserModelAttributes>, comparePassword: boolean = true): UserModelAttributes => ({
     id: 'f959cd1f-b68d-4606-8b50-4a347dfbc7ab',
     name: 'test',
     email: 'test@email.com',
@@ -153,3 +148,13 @@ const createMockUser = (overrides?: object, comparePassword: boolean = true) => 
     update: jest.fn((object: object) => { createMockUser(object) }),
     destroy: jest.fn(() => (null)),
 });
+
+
+interface UserModelAttributes {
+    id: string;
+    name: string;
+    email: string;
+    comparePassword: (password: string) => boolean;
+    update: jest.Mock<any, any>;
+    destroy: jest.Mock<any, any>;
+}
